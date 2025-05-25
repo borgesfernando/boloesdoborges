4765040e82bca5589ef1a4f69c518fa894b51b10
Fernando Borges <32992145+borgesfernando@users.noreply.github.com>
2025-05-25 10:26:21 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,104 +1,113 @@
-const CONF_PROJETOS = {
-  'Lotofácil da Independência': {
-    nome: 'Lotofácil da Independência',
-    tipoProj: "PERMANENTE",
-    planilhaId: '1XhW8VuPq5vj3pNDqrHLEn4nHTXU4g8lQyrJoJo1STno',
-    abaParticipantes: 'Relacao_Participantes',
-    apuracao: '07/09/2025',
-    valorParcela: 30,
-    parcelasTotais: 8,
-    diaPix: 7,
-    idLink: '1hkwm2BOOvnneq8m5KJ9x8kxJ1xvzTj9BOgrsLfFml0I',
-    mensagemUrgente: "ÚLTIMA CHAMADA p/ entrada no Bolão da LF da Independência!",
-    dataLimite: '07/08/2025',
-    mesInicio: '01',
-    mesFim: '08',
-    logTag: 'LF_INDEP',
-    intervalosAba: 'A1:P2, A4:P5, P6:P8',
-    grupoWhatsApp: 'https://chat.whatsapp.com/KXGrVCwQcqi4xZPh7gD7Vd',
-    idFolderPrestContas: "1kh5R6_vbrt_XRBZ9c3JnGlmcs94oA3-D",
-
-    // Estilo visual expandido
-    rgbProjeto: '128, 53, 148',
-    corPrimaria: '128, 53, 148', // Roxo Lotofácil
-    corHex: '#803594',
-    gradiente: 'linear-gradient(to right, rgb(128,53,148), rgb(239,118,94))',
-    corTextoClaro: true
+const PROJETOS = {
+  especiais: {
+    template: 'especiais',
+    projetos: [
+        {
+        id: 'lf-independencia',
+        nome: 'Lotofácil da Independência',
+        cota: 240,
+        valorMes: 30,
+        parcelas: 8,
+        diaPix: 7,
+        mesInicio: '01',
+        mesFim: '8',
+        dataLimite: '07/08/2025',
+        dataSorteio: '07/09/2025',
+        corPrimaria: '128, 53, 148', 
+        corHex: '#803594',
+        gradiente: 'linear-gradient(to right, rgb(128,53,148), rgb(239,118,94))',
+      },
+      {
+        id: 'quina-saojoao',
+        nome: 'Quina de São João',
+        cota: 180,
+        valorMes: 30,
+        parcelas: 6,
+        diaPix: 8,
+        mesInicio: '01',
+        mesFim: '06',
+        dataLimite: '08/06/2025',
+        dataSorteio: '24/06/2025',
+        corPrimaria: '0, 93, 164', 
+        corHex: '#005DA4',
+        gradiente: 'linear-gradient(to right, rgb(0,93,164), rgb(0,153,218))',
+      },
+      {
+        id: 'ds-pascoa',
+        nome: 'Dupla Sena de Páscoa',
+        cota: 120,
+        valorMes: 30,
+        parcelas: 4,
+        diaPix: 9,
+        mesInicio: '01',
+        mesFim: '04',
+        dataLimite: '09/04/2025',
+        dataSorteio: '19/04/2025',
+        corPrimaria: '166, 42, 82', 
+        corHex: '#A62A52',
+        gradiente: 'linear-gradient(to right, rgb(166,42,82), rgb(215,25,32))',
+      },
+      {
+        id: 'mega-virada',
+        nome: 'Super Bolão para a Mega da Virada',
+        cota: 600,
+        valorMes: 50,
+        parcelas: 12,
+        diaPix: 10,
+        mesInicio: '01',
+        mesFim: '12',
+        dataLimite: '10/12/2025',
+        dataSorteio: '31/12/2025',
+        corPrimaria: '0, 171, 103', 
+        corHex: '#00AB67',
+        gradiente: 'linear-gradient(to right, rgb(0,171,103), rgb(128,195,66))',
+      },
+    ]
   },
-
-  'Quina de São João': {
-    nome: 'Quina de São João',
-    tipoProj: "PERMANENTE",
-    planilhaId: '1ISdA7RsIFGoe6MpJ7uZEbLZKroappp6w5Avy7FELddg',
-    apuracao: '24/06/2025',
-    valorParcela: 30,
-    parcelasTotais: 6,
-    diaPix: 8,
-    idLink: '1pJs3LhoQX0ysLO5eQ74Ao5o7KoXGDsFGx_mgv6gGWzY',
-    mensagemUrgente: "ÚLTIMA CHAMADA p/ entrada no Bolão da Quina de São João!",
-    dataLimite: '08/06/2025',
-    mesInicio: '01',
-    mesFim: '06',
-    logTag: 'QSJ',
-    intervalosAba: 'A1:P2, A4:P5, P6:P8',
-    grupoWhatsApp: 'https://chat.whatsapp.com/BGnpJk9BBUALr8DydZkg8R',
-    idFolderPrestContas: "1fU9WN1AsLhih7as9XWgAkPZ-TuJR4bBf",
-
-    rgbProjeto: '0, 93, 164',
-    corPrimaria: '0, 93, 164', // Azul Quina
-    corHex: '#005DA4',
-    gradiente: 'linear-gradient(to right, rgb(0,93,164), rgb(0,153,218))',
-    corTextoClaro: true
+  mensais: {
+    template: 'mensais',
+    projetos: [
+      {
+        id: 'quina-mensal',
+        nome: 'Quina Mensal',
+        cotaMensal: 60,
+        ciclo: '24',
+        corPrimaria: '0, 93, 164', 
+        corHex: '#005DA4',
+        gradiente: 'linear-gradient(to right, rgb(0,93,164), rgb(0,153,218))',
+      },
+      {
+        id: 'lf-mensal',
+        nome: 'Lotofácil Mensal',
+        cotaMensal: 100,
+        ciclo: '24',
+        corPrimaria: '128, 53, 148', 
+        corHex: '#803594',
+        gradiente: 'linear-gradient(to right, rgb(128,53,148), rgb(239,118,94))',
+      },
+    ]
   },
-
-  'Dupla de Páscoa': {
-    nome: 'Dupla Sena de Páscoa',
-    tipoProj: "PERMANENTE",
-    planilhaId: '19m4z8ipZRPJtgbNbvtrtcKkH93rb4bzWYiSyjMbw8f0',
-    apuracao: '19/04/2025',
-    valorParcela: 30,
-    parcelasTotais: 4,
-    diaPix: 9,
-    idLink: '1S4TjPC9gNcSmkQhzUTFdtF4LFAMbX5HsT9AFzi7zkdQ',
-    mensagemUrgente: "ÚLTIMA CHAMADA p/ entrada no Bolão da DS de Páscoa!",
-    dataLimite: '09/04/2025',
-    mesInicio: '01',
-    mesFim: '04',
-    logTag: 'DSP',
-    intervalosAba: 'A1:P2, A4:P5, P6:P8',
-    grupoWhatsApp: 'https://chat.whatsapp.com/G0ESXDad9Wz19Qdf3N98ZW',
-    idFolderPrestContas: "1OVKgiz8PjHh_C14TU8tqyGWgFxxNgeEK",
-
-    rgbProjeto: '166, 42, 82',
-    corPrimaria: '166, 42, 82', // Vinho Dupla Sena
-    corHex: '#A62A52',
-    gradiente: 'linear-gradient(to right, rgb(166,42,82), rgb(215,25,32))',
-    corTextoClaro: true
-  },
-
-  'Mega da Virada (SB)': {
-    nome: 'Mega da Virada (SB)',
-    tipoProj: "PERMANENTE",
-    planilhaId: '1rhbTxlXQBcsgZ2kn2YMz-p2Q_7MLMDBiz8ZwiWBAb1U',
-    apuracao: '31/12/2025',
-    valorParcela: 50,
-    parcelasTotais: 12,
-    diaPix: 10,
-    idLink: '1W3CFSgaKHlral5yKhaMPtcYdzSg2HqZoYO8NO_1rJV8',
-    mensagemUrgente: "ÚLTIMA CHAMADA p/ entrada no Bolão da Mega da Virada!",
-    dataLimite: '10/12/2025',
-    mesInicio: '01',
-    mesFim: '12',
-    logTag: 'SB2025',
-    intervalosAba: 'A1:P2, A4:P5, P6:P8',
-    grupoWhatsApp: 'https://chat.whatsapp.com/Kxo6sKmcJBX1XpOqc5kz2p',
-    idFolderPrestContas: "1WL1HfPE2PSvW88efrBZLxauJ6rRlN5Yn",
-
-    rgbProjeto: '0, 171, 103',
-    corPrimaria: '0, 171, 103', // Verde Mega-Sena
-    corHex: '#00AB67',
-    gradiente: 'linear-gradient(to right, rgb(0,171,103), rgb(128,195,66))',
-    corTextoClaro: true
+  acumulados: {
+    template: 'acumulados',
+    projetos: [
+      {
+        id: 'mega-acumulada',
+        nome: 'Mega Sena Acumulada +50Mi',
+        valor: 20,
+        minimo: 50,
+        corPrimaria: '0, 171, 103', 
+        corHex: '#00AB67',
+        gradiente: 'linear-gradient(to right, rgb(0,171,103), rgb(128,195,66))',
+      },
+            {
+        id: 'quina-acumulada',
+        nome: 'Quina Acumulada +40Mi',
+        valor: 20,
+        minimo: 40,
+        corPrimaria: '0, 93, 164', 
+        corHex: '#005DA4',
+        gradiente: 'linear-gradient(to right, rgb(0,93,164), rgb(0,153,218))',
+      },
+    ]
   }
 };
-
