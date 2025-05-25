00bdd38480f06c70f00c80e87b1da1f64a9ebc31
Fernando Borges <32992145+borgesfernando@users.noreply.github.com>
2025-05-25 09:30:37 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,104 @@
+const CONF_PROJETOS = {
+  'Lotofácil da Independência': {
+    nome: 'Lotofácil da Independência',
+    tipoProj: "PERMANENTE",
+    planilhaId: '1XhW8VuPq5vj3pNDqrHLEn4nHTXU4g8lQyrJoJo1STno',
+    abaParticipantes: 'Relacao_Participantes',
+    apuracao: '07/09/2025',
+    valorParcela: 30,
+    parcelasTotais: 8,
+    diaPix: 7,
+    idLink: '1hkwm2BOOvnneq8m5KJ9x8kxJ1xvzTj9BOgrsLfFml0I',
+    mensagemUrgente: "ÚLTIMA CHAMADA p/ entrada no Bolão da LF da Independência!",
+    dataLimite: '07/08/2025',
+    mesInicio: '01',
+    mesFim: '08',
+    logTag: 'LF_INDEP',
+    intervalosAba: 'A1:P2, A4:P5, P6:P8',
+    grupoWhatsApp: 'https://chat.whatsapp.com/KXGrVCwQcqi4xZPh7gD7Vd',
+    idFolderPrestContas: "1kh5R6_vbrt_XRBZ9c3JnGlmcs94oA3-D",
+
+    // Estilo visual expandido
+    rgbProjeto: '128, 53, 148',
+    corPrimaria: '128, 53, 148', // Roxo Lotofácil
+    corHex: '#803594',
+    gradiente: 'linear-gradient(to right, rgb(128,53,148), rgb(239,118,94))',
+    corTextoClaro: true
+  },
+
+  'Quina de São João': {
+    nome: 'Quina de São João',
+    tipoProj: "PERMANENTE",
+    planilhaId: '1ISdA7RsIFGoe6MpJ7uZEbLZKroappp6w5Avy7FELddg',
+    apuracao: '24/06/2025',
+    valorParcela: 30,
+    parcelasTotais: 6,
+    diaPix: 8,
+    idLink: '1pJs3LhoQX0ysLO5eQ74Ao5o7KoXGDsFGx_mgv6gGWzY',
+    mensagemUrgente: "ÚLTIMA CHAMADA p/ entrada no Bolão da Quina de São João!",
+    dataLimite: '08/06/2025',
+    mesInicio: '01',
+    mesFim: '06',
+    logTag: 'QSJ',
+    intervalosAba: 'A1:P2, A4:P5, P6:P8',
+    grupoWhatsApp: 'https://chat.whatsapp.com/BGnpJk9BBUALr8DydZkg8R',
+    idFolderPrestContas: "1fU9WN1AsLhih7as9XWgAkPZ-TuJR4bBf",
+
+    rgbProjeto: '0, 93, 164',
+    corPrimaria: '0, 93, 164', // Azul Quina
+    corHex: '#005DA4',
+    gradiente: 'linear-gradient(to right, rgb(0,93,164), rgb(0,153,218))',
+    corTextoClaro: true
+  },
+
+  'Dupla de Páscoa': {
+    nome: 'Dupla Sena de Páscoa',
+    tipoProj: "PERMANENTE",
+    planilhaId: '19m4z8ipZRPJtgbNbvtrtcKkH93rb4bzWYiSyjMbw8f0',
+    apuracao: '19/04/2025',
+    valorParcela: 30,
+    parcelasTotais: 4,
+    diaPix: 9,
+    idLink: '1S4TjPC9gNcSmkQhzUTFdtF4LFAMbX5HsT9AFzi7zkdQ',
+    mensagemUrgente: "ÚLTIMA CHAMADA p/ entrada no Bolão da DS de Páscoa!",
+    dataLimite: '09/04/2025',
+    mesInicio: '01',
+    mesFim: '04',
+    logTag: 'DSP',
+    intervalosAba: 'A1:P2, A4:P5, P6:P8',
+    grupoWhatsApp: 'https://chat.whatsapp.com/G0ESXDad9Wz19Qdf3N98ZW',
+    idFolderPrestContas: "1OVKgiz8PjHh_C14TU8tqyGWgFxxNgeEK",
+
+    rgbProjeto: '166, 42, 82',
+    corPrimaria: '166, 42, 82', // Vinho Dupla Sena
+    corHex: '#A62A52',
+    gradiente: 'linear-gradient(to right, rgb(166,42,82), rgb(215,25,32))',
+    corTextoClaro: true
+  },
+
+  'Mega da Virada (SB)': {
+    nome: 'Mega da Virada (SB)',
+    tipoProj: "PERMANENTE",
+    planilhaId: '1rhbTxlXQBcsgZ2kn2YMz-p2Q_7MLMDBiz8ZwiWBAb1U',
+    apuracao: '31/12/2025',
+    valorParcela: 50,
+    parcelasTotais: 12,
+    diaPix: 10,
+    idLink: '1W3CFSgaKHlral5yKhaMPtcYdzSg2HqZoYO8NO_1rJV8',
+    mensagemUrgente: "ÚLTIMA CHAMADA p/ entrada no Bolão da Mega da Virada!",
+    dataLimite: '10/12/2025',
+    mesInicio: '01',
+    mesFim: '12',
+    logTag: 'SB2025',
+    intervalosAba: 'A1:P2, A4:P5, P6:P8',
+    grupoWhatsApp: 'https://chat.whatsapp.com/Kxo6sKmcJBX1XpOqc5kz2p',
+    idFolderPrestContas: "1WL1HfPE2PSvW88efrBZLxauJ6rRlN5Yn",
+
+    rgbProjeto: '0, 171, 103',
+    corPrimaria: '0, 171, 103', // Verde Mega-Sena
+    corHex: '#00AB67',
+    gradiente: 'linear-gradient(to right, rgb(0,171,103), rgb(128,195,66))',
+    corTextoClaro: true
+  }
+};
+
